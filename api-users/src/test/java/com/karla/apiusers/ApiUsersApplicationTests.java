79e55f9c7e1de32e4d2154f51c46e7a3e3c40346
Karla Sequen <karlasequen@gmail.com>
2025-04-22 19:00:02 -0600

    Init projects for api

---
+++ api-users/src/test/java/com/karla/apiusers/ApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karla.apiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
